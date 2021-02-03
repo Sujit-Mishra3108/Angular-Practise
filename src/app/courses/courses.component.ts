22aebef4c1473ce5b26aae7653ca5f3803db44c2
Sujit-Mishra3108 <Sujitmishra77797@gmail.com>
2021-02-03 17:04:14 +0530

    Event Bubbling

--- src/app/courses/courses.component.ts
+++ src/app/courses/courses.component.ts
@@ -3,21 +3,27 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-courses',
   template: `
-  <button class="btn btn-primary" [class.active]="isActive" >Class Binding</button> 
-  <button [style.backgroundColor]="isActive ? 'lightblue': 'white'" >Style Binding</button> 
-  <button (click)="onSave()">Event Binding</button>
-  `,
+          <div (click)="onDivClicked($event)" >
+            <button class="btn btn-primary" [class.active]="isActive" >Class Binding</button> 
+            <button [style.backgroundColor]="isActive ? 'lightblue': 'white'" >Style Binding</button> 
+            <button (click)="onSave($event)">Event Binding</button>
+          </div>`,
   // templateUrl: './courses.component.html',
   styleUrls: ['./courses.component.css']
 })
 export class CoursesComponent{
 
-  title = "list of Courses";
-  imageUrl = "http://lorempixel.com/400/200";
-  //colSpan = 2;
-  isActive = true;  // based on this value button will behave
+    title = "list of Courses";
+    imageUrl = "http://lorempixel.com/400/200";
+    //colSpan = 2;
+    isActive = true;  // based on this value button will behave
 
-  onSave(){
-    console.log("Button was clicked");
-  }
+    onSave($event: any){
+      console.log("Button was clicked", $event);
+    }
+
+    onDivClicked($event: any){
+      console.log("Div was clicked", $event);
+      
+    }
 }
