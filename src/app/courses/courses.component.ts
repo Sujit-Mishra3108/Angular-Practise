0cc6a0546e72014a34edbed375e96dc0378a6c0f
Sujit-Mishra3108 <Sujitmishra77797@gmail.com>
2021-02-03 17:47:32 +0530

    By using Template Variable

--- src/app/courses/courses.component.ts
+++ src/app/courses/courses.component.ts
@@ -3,35 +3,14 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-courses',
   template: `
-          <div (click)="onDivClicked($event)" >
-            <button class="btn btn-primary" [class.active]="isActive" >Class Binding</button> 
-            <button [style.backgroundColor]="isActive ? 'lightblue': 'white'" >Style Binding</button> 
-            <button (click)="onSave($event)">Event Binding</button>
-            <hr/>
-            <input (keyUp.enter)="onKeyUp($event)">
-            <input (keyUp.enter)="onKeyUp()">
-          </div>`,
+            <input #email (keyUp.enter)="onKeyUp(email.value)">
+          `,
   // templateUrl: './courses.component.html',
   styleUrls: ['./courses.component.css']
 })
 export class CoursesComponent{
 
-    title = "list of Courses";
-    imageUrl = "http://lorempixel.com/400/200";
-    //colSpan = 2;
-    isActive = true;  // based on this value button will behave
-
-    onKeyUp($event: any){
-       console.log($event.target.value);
-    }
-
-    onDivClicked($event: any){
-      console.log("Div was clicked", $event);
-    }
-
-    onSave($event: any){
-      $event.stopPropagation();
-      console.log("Button was clicked", $event);
-    }
-
+    onKeyUp(email : any){
+      console.log(email);
+   }
 }
