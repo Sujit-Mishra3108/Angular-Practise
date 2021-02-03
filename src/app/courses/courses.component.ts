af339c629bd0a5063ac7e442878115e51136ba8d
Sujit-Mishra3108 <Sujitmishra77797@gmail.com>
2021-02-03 16:37:32 +0530

    Class Binding

--- src/app/courses/courses.component.ts
+++ src/app/courses/courses.component.ts
@@ -2,13 +2,16 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-courses',
-  templateUrl: './courses.component.html',
+  template: `
+  <button class="btn btn-primary  [class.active]='isActive' ">Save</button>  
+  `, //class binding
+  // templateUrl: './courses.component.html',
   styleUrls: ['./courses.component.css']
 })
 export class CoursesComponent{
 
   title = "list of Courses";
   imageUrl = "http://lorempixel.com/400/200";
-  colSpan = 2;
-
+  //colSpan = 2;
+  isActive = true;  // based on this value button will behave
 }
