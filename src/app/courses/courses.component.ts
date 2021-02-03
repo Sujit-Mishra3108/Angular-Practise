5f20324f59fa506aacc5198255c68d14c3f28d44
Sujit-Mishra3108 <Sujitmishra77797@gmail.com>
2021-02-03 16:56:27 +0530

    ignore

--- src/app/courses/courses.component.ts
+++ src/app/courses/courses.component.ts
@@ -3,8 +3,10 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-courses',
   template: `
-  <button class="btn btn-primary  [class.active]='isActive' ">Save</button>  
-  `, //class binding
+  <button class="btn btn-primary" [class.active]="isActive" >Class Binding</button> 
+  <button [style.backgroundColor]="isActive ? 'lightblue': 'white'" >Style Binding</button> 
+  <button (click)="onSave()">Event Binding</button>
+  `,
   // templateUrl: './courses.component.html',
   styleUrls: ['./courses.component.css']
 })
@@ -14,4 +16,8 @@ export class CoursesComponent{
   imageUrl = "http://lorempixel.com/400/200";
   //colSpan = 2;
   isActive = true;  // based on this value button will behave
+
+  onSave(){
+    console.log("Button was clicked");
+  }
 }
